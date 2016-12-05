ae04e26225f87abf2bf7d3c50e7749a8bee6c2f5
yhongo <yutaro_hongo@monex.co.jp>
2016-12-05 15:30:26 +0900

    Finished Arithmetic fomula functions.

--- Cryptarithmetic.py
+++ Cryptarithmetic.py
@@ -27,33 +27,94 @@ def valid(f):
     except ArithmeticError:
         return False
 
+
 examples = """TWO + TWO == FOUR
 A**2 + B**2 == C**2
 A**2 + BE**2 == BY**2
 X / X == X
 A**N + B**N ++ C**N and N > 1
+ATOM**0.5 == A + TO + M
+GLITTERS is not GLOD
+ONE < TWO and FOUR < FIVE
+ONE < TWO < THREE
+RAMN == R ** 3 + RM**3 == N**3 + RX**3
+sum(range(AA)) == BB
+sum(range(POP)) == BOBO
+ODD + ODD == EVEN
+PLUTO not in set([PLANETS])""".splitlines()
 
-"""
+def timedcall(fn, *args):
+    """ Call function with args; return the time in seconds adn result """
+    t0 = time.clock()
+    result = fn(*args)
+    t1 = time.clock()
+    return t1-t0, result
+
+
+def average(numbers):
+    """ Return the average (arithmetic mean) of a sequence of numbers  """
+    return sum(numbers) / float(len(numbers))
+
 
 def timedcalls(n, fn, *args):
-    if isinstance(n, (int,long)):
+    """Call fn(*args) repeatedly: n times if n is an int, or up to
+    n seconds if n is a float; return the min, avg, and max time"""
+    if isinstance(n, int):
         times = [timedcalls(fn,*args)[0] for _ in range(n)]
 
     else:
-        total, times = 0, []
-        while total <= n:
-            time = timedcalls(fn, *args)[0]
-            times.append(time)
-            total += time
-
+        times = []
+        while sum(times) < n:
+            times.append(timedcall(fn, *args)[0])
     return min(times), average(times), max(times)
 
 
+def compile_word(word):
+    """Compile a word of uppercase letters as numeric digits.
+    E.g., compile_word('YOU') => '(1*U+10*O+100*Y)'
+    Non-uppercase words unchanged: compile_word('+') => '+'"""
+    if word.isupper():
+        terms = [('%s*%s' % (10**i, d)) for (i, d) in enumerate(word[::-1])]
+        return '(' + '+'.join(terms) + ')'
+    else:
+        return word
 
+
+def compile_formula(formula, verbose=False):
+    """Compile formula into a function Also return letters found, as a str,
+    in same order as params of function For example, 'YOU == ME**2' returns
+    (lambda Y,M,E,U,O): (U+10*0+100*Y) == (E+10*M)**2, 'YMEUO' """
+    letters = ''.join(set(re.findall('[A-Z]', formula)))
+    parms = ', '.join(letters)
+    tokens = map(compile_word, re.split('([A-Z]+)', formula))
+    body = ''.join(tokens)
+    f = 'lambda %s: %s' % (parms, body)
+    if verbose: print f
+    return eval(f), letters
+
+
+def faster_solve(formula):
+    """"Given a formula like 'ODD + ODD = EVEN', fill in digits
+      to solve it. Input formula is a string; out put is a digit-filled- in string or None"""
+    f, letters = compile_formula(formula)
+    for digits in itertools.permutations((1,2,3,4,5,6,7,8,9,0), len(letters)):
+        try:
+            if f(*digits) is True:
+                table = string.maketrans(letters, ''.join(map(str, digits)))
+                return formula.translate(table)
+        except ArithmeticError:
+            pass
 
 
 def test():
     t0 = time.clock()
     for example in examples:
-        print; pring 13*'', example
-        print '%6.4f sed:  %s ' % timedcall(solve,example)
+        print 13*'', example
+        print '%6.4f sed:  %s ' % timedcall(faster_solve, example)
+
+
+print(faster_solve('YOU == ME**2'))
+
+print test()
+
+print(set([6315237]))
